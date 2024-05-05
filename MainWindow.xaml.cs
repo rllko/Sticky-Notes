90d7c514acb242aee107e82ce5c3b0ba5e442f63
newrlkko <ric.fon@ua.pt>
2024-05-05 20:07:57 +0100

    Finished Main screen, Started Note Editor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using DapperGenericRepository.Repository;
+using StickyNotes.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,57 @@ namespace StickyNotes
     /// </summary>
     public partial class MainWindow : Window
     {
+        GenericRepository<NoteDetailDto> NoteRepository = new();
+        List<NoteDetailDto> notes = new();
         public MainWindow()
         {
             InitializeComponent();
+            NoteDetailDto note = new NoteDetailDto();
+            note.Content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+            notes.Add(note);
+            this.data.ItemsSource = notes;
+        }
+
+        private void buttonSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Dragbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+                e.Handled = true;
+            }
+        }
+
+        private void Window_Main_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+                e.Handled = true;
+            }
+        }
+
+        private void DataContainer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
         }
     }
 }
